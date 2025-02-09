0a28cda5fe6e2dfa502e3c7ee6b2670e13ae8a1c
ServerCrashed <aks3045@proton.me>
2025-02-09 15:31:51 +0530

    Create the playGame function
    Runs the playRound function 5 times,
    keeping track of both players' scores
    and announces the overall winner at
    the end.

--- script.js
+++ script.js
@@ -9,49 +9,57 @@ const message = `Enter the number pertaining to your choice:-
 `;
 let getHumanChoice = () => a[+prompt(message)];
 
-let humanScore=0, computerScore=0;
+function playGame(){
+    let humanScore=0, computerScore=0;
 
-function lose(hS, cS){
-    console.log(`You lose! ${cS} beats ${hS}.`);
+    function playRound(hC, cC){
+        if(hC===cC) console.log("That's a tie!");
+
+        else if(hC==="rock" && cC==="scissors") {
+            win(hC, cC);
+            humanScore+=1;
+        }
+        else if(hC==="rock" && cC==="paper") {
+            lose(hC, cC); 
+            computerScore+=1;
+        }
+
+        else if(hC==="paper" && cC==="rock") {
+            win(hC, cC);
+            humanScore+=1;
+        }
+        else if(hC==="paper" && cC==="scissors") {
+            lose(hC, cC); 
+            computerScore+=1;
+        }
+
+        else if(hC==="scissors" && cC==="paper") {
+            win(hC, cC);
+            humanScore+=1;
+        }
+        else if(hC==="scissors" && cC==="rock") {
+            lose(hC, cC); 
+            computerScore+=1;
+        }
+    }
+    function lose(hS, cS){
+        console.log(`You lose! ${cS} beats ${hS}.`);
+    }
+    function win(hS, cS){
+        console.log(`You win! ${hS} beats ${cS}.`);
+    }
+
+    for(let i=0; i<5; i++){
+        let humanSelection = getHumanChoice();
+        let compSelection = getComputerChoice();
+        console.log(compSelection);
+
+        playRound(humanSelection, compSelection);
+    }
+
+    if(humanScore > computerScore) console.log("You won the game!");
+    else if(humanScore < computerScore) console.log("You lost!");
+    else console.log("Tied!");
 }
 
-function win(hS, cS){
-    console.log(`You win! ${hS} beats ${cS}.`);
-}
-
-function playRound(hC, cC){
-    if(hC===cC) console.log("That's a tie!");
-
-    else if(hC==="rock" && cC==="scissors") {
-        win(hC, cC);
-        humanScore+=1;
-    }
-    else if(hC==="rock" && cC==="paper") {
-        lose(hC, cC); 
-        computerScore+=1;
-    }
-
-    else if(hC==="paper" && cC==="rock") {
-        win(hC, cC);
-        humanScore+=1;
-    }
-    else if(hC==="paper" && cC==="scissors") {
-        lose(hC, cC); 
-        computerScore+=1;
-    }
-
-    else if(hC==="scissors" && cC==="paper") {
-        win(hC, cC);
-        humanScore+=1;
-    }
-    else if(hC==="scissors" && cC==="rock") {
-        lose(hC, cC); 
-        computerScore+=1;
-    }
-}
-
-let humanSelection = getHumanChoice();
-let compSelection = getComputerChoice();
-console.log(compSelection);
-
-playRound(humanSelection, compSelection);
+playGame();
